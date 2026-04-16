cb54ea1d8c648a2025ce985087ef676cd38eb034
Ghaida <alsabtighaida@gmail.com>
2026-04-16 07:32:45 +0000

    added: main.h and _printf.c

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_string(char *p);
+int print_int(int i);
+int print_unsigned(unsigned int num);
+
+#endif
